b22511a539f28a4062a9af274c36a8b1f00af8dd
tiyo5151 <s1F102400224@iniad.org>
2024-08-07 05:59:24 +0900

    fixed

---
+++ src/component/mybutton.tsx
@@ -0,0 +1,8 @@
+function Mybotton() {
+  return <button>I&apos;m a button</button>;
+}
+
+export const myVariable = Math.sqrt(2);
+export default Mybotton;
+export {};
+// 何も送らないという意味
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,7 +6,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Create Next App</title>
+        <title>newReactPractice</title>
       </Head>
       <Component {...pageProps} />
     </>
