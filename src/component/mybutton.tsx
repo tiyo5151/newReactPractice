edad3cde04e82bbd6225ddaf1585de47bc003baa
tiyo5151 <s1F102400224@iniad.org>
2024-08-09 00:49:25 +0900

    ちゃりこぎに行く

--- src/component/mybutton.tsx
+++ src/component/mybutton.tsx
@@ -1,15 +1,12 @@
 import useCount from '../hooks/useCount';
+import styles from '../pages/index.module.css';
 function Mybotton() {
   const { count, increment, decrement } = useCount();
   return (
-    <div>
-      <p>Count:{count}</p>
-      <button onClick={() => decrement()} className="decrementButtomn">
-        Click me!
-      </button>
-      <button onClick={() => increment()} className="incrementButton">
-        Click Me!
-      </button>
+    <div className="styles.counter">
+      <p className={styles.big}>Count:{count}</p>
+      <button onClick={() => decrement()}>Click me!</button>
+      <button onClick={() => increment()}>Click Me!</button>
     </div>
   );
 }
